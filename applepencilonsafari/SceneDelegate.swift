2597062022e33e5aacffdc096d3bf39b3854f41b
piano.jang <vusghwkd@snu.ac.kr>
2026-03-19 20:03:00 +0900

    Initial Commit

---
+++ applepencilonsafari/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  applepencilonsafari
+//
+//  Created by 편호장 on 3/19/26.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ applepencilonsafariTests/applepencilonsafariTests.swift
@@ -0,0 +1,17 @@
+//
+//  applepencilonsafariTests.swift
+//  applepencilonsafariTests
+//
+//  Created by 편호장 on 3/19/26.
+//
+
+import Testing
+@testable import applepencilonsafari
+
+struct applepencilonsafariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
